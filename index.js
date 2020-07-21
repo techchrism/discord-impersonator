1844b640ed72ac56bbdf1102fc30f4f378854b93
Techdoodle <26680599+TheTechdoodle@users.noreply.github.com>
2020-07-20 17:52:31 -0700

    add config generation and parsing

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
+async function writeDefaultConfig()
+{
+    return fs.writeFile('config.json', JSON.stringify({
+        token: ''
+    }, null, 4));
+}
+
+async function loadConfig()
+{
+    let fileText = '';
+    try
+    {
+        fileText = await fs.readFile('config.json', 'utf8');
+    }
+    catch(e)
+    {
+        console.error('Config file not found! Creating default config...');
+        await writeDefaultConfig();
+        throw e;
+    }
+    
+    try
+    {
+        return JSON.parse(fileText);
+    }
+    catch(e)
+    {
+        const newName = `config-broken-${Date.now()}.json`;
+        console.error(`Could not parse config! Moved to ${newName}`);
+        await fs.rename('config.json', newName);
+        await writeDefaultConfig();
+        throw e;
+    }
+}
+
+loadConfig().then(config =>
+{
+    console.log('Parsed config');
+    console.log(config);
+}).catch(e =>
+{
+
+});
